a84a38383833531b753d4f0702c470ddd5dc3d6a
Benjamin Cellini <31375763+boc5244@users.noreply.github.com>
2019-08-28 18:02:33 -0400

    BatchBenifly is more user friendly, update ReadMe

--- scripts/BatchBenifly.py
+++ scripts/BatchBenifly.py
@@ -4,19 +4,14 @@ from fileimport import FileImport
 import time
 
 # USER: set these variables
-#-------------------------------------------------------------------------
-mainroot = "C:\Users/boc5244\PycharmProjects\Benifly" # location of Benifly root
-root = 'H:\EXPERIMENTS\Experiment_Chirp_Walking\mat'  # folder with MATLAB video files
-filespec = 'fly_1_trial_2_Amp_7.5.mat' # files to track
-targetdir = 'C:\Users/boc5244\Documents/temp\out' # where to save output data & video
-vidname = 'Vid' # name of video variable in MATLAB .mat file
-
-# mainroot = "C:\Users\BC\PycharmProjects\Benifly" # location of Benifly root
-# root = 'Q:/temp'  # folder with MATLAB video files
-# filespec = 'fly_1_trial_1_SOS.mat' # files to track
-# targetdir = 'Q:/temp\out' # where to save output data & video
-# vidname = 'vidData' # name of video variable in MATLAB .mat file
-#-------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
+mainroot = "C:\Users\BC\PycharmProjects\Benifly" # location of Benifly root
+#-----------------------------------------------------------------------------------------
+root = 'Q:/temp'  # folder with MATLAB video files
+filespec = 'fly_1_trial_1_SOS.mat' # files to track
+targetdir = 'Q:/temp\out' # where to save output data & video
+vidname = 'vidData' # name of video variable in MATLAB .mat file
+##-----------------------------------------------------------------------------------------
 
 Benifly = MainWindow(mainroot) # create Benifly instance
 VID = FileImport() # create FileImport instance
